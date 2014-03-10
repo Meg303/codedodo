e2b016b70cffb23fa7a99b8a8f1eef563e46220a
Meg303 <megrhinehart@gmail.com>
2014-03-10 19:17:54 -0400

    repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codedodo::Application.config.secret_key_base = '5dc0d6044afcbd7e93fc7285940f974797e04268eafffa2c04bda7bfe066c7537a7d089a27c07e860d949e4f5bd8938e75844b2fded7b4ec35dd463a59be256d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codedodo::Application.config.session_store :cookie_store, key: '_codedodo_session'
